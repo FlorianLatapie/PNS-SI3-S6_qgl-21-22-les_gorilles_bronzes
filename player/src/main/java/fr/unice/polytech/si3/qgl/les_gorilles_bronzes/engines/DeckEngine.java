b3a38812012ff171d6ed0a476668bec228b08070
marius <marius.lesaulnier@etu.unice.fr>
2022-02-03 10:57:48 +0100

    Création du DeckEngine #4

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/les_gorilles_bronzes/engines/DeckEngine.java
@@ -0,0 +1,26 @@
+package fr.unice.polytech.si3.qgl.les_gorilles_bronzes.engines;
+
+public class DeckEngine {
+    private int nbOfSailors;
+    private int nbOfOar;
+
+    public int getNbOfSailors() {
+        return nbOfSailors;
+    }
+
+    public void setNbOfSailors(int nbOfSailors) {
+        this.nbOfSailors = nbOfSailors;
+    }
+
+    public int getNbOfOar() {
+        return nbOfOar;
+    }
+
+    public void setNbOfOar(int nbOfOar) {
+        this.nbOfOar = nbOfOar;
+    }
+
+    public DeckEngine(){
+
+    }
+}
