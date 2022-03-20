9a750ebab417ee8e4a08edfb4f8c109d7c3bec4b
Florian LATAPIE <florian.latapie@etu.unice.fr>
2022-03-20 01:18:36 +0100

    ajout d'un afficheur simple du pathfinding en JFrame #14

--- player/src/main/java/fr/unice/polytech/si3/qgl/les_gorilles_bronzes/Cockpit.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/les_gorilles_bronzes/Cockpit.java
@@ -20,6 +20,7 @@ public class Cockpit implements ICockpit {
     private GlobalEngine globalEngine;
     private static Logger logger = Logger.getLogger(Cockpit.class.getName());
     private static List<String> logs = new ArrayList<>();
+    private static boolean displayGraph;
 
     public Cockpit() {
         /**
@@ -29,10 +30,15 @@ public class Cockpit implements ICockpit {
         log("Cockpit initialized");
     }
 
+    public Cockpit(boolean displayGraph) {
+        this();
+        this.displayGraph = displayGraph;
+    }
+
     public void initGame(String jsonGame) {
         try {
             this.initGame = OBJECT_MAPPER.readValue(jsonGame, InitGame.class);
-            this.globalEngine = new GlobalEngine(initGame);
+            this.globalEngine = new GlobalEngine(initGame, displayGraph);
         } catch (JsonProcessingException e) {
             e.getMessage();
         }
